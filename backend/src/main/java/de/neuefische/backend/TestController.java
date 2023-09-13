f3208e030eddeab82a539ecbce6464bb970648a9
marce <marcell.dechant@neuefische.de>
2023-09-13 12:29:20 +0200

    Update deployment

--- backend/src/main/java/de/neuefische/backend/TestController.java
+++ backend/src/main/java/de/neuefische/backend/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World2";
     }
 }
